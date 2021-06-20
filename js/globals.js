cb87e193a5b1d21aea5d53aead79764b3021159d
Nithin S Varrier <nithin486@hotmail.com>
2021-06-20 16:12:22 +0530

    Avoid removing active class on index page

--- js/globals.js
+++ js/globals.js
@@ -1,10 +1,13 @@
 $(function () {
   $("#header").load("components/navbar.html", function (data) {
-    $("li").removeClass("active");
-    var val = $("#main").attr("data-under");
-    $('[data-menuanchor$="' + val + '"]')
-      .first()
-      .addClass("active");
+    if ($("#main").length) {
+      $("li").removeClass("active");
+      var val = $("#main").attr("data-under");
+      $('[data-menuanchor$="' + val + '"]')
+        .first()
+        .addClass("active");
+    }
   });
+
   $("#social-icons").load("components/social-icons.html");
 });
