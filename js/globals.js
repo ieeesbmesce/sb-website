de0c2060d9336df56a9ac11e71d503ecd3a68803
Nithin S Varrier <nithin486@hotmail.com>
2021-06-20 14:37:32 +0530

    Added automatic class adding based on data-under attribute

--- js/globals.js
+++ js/globals.js
@@ -1,4 +1,10 @@
 $(function () {
-  $("#header").load("components/navbar.html");
+  $("#header").load("components/navbar.html", function (data) {
+    $("li").removeClass("active");
+    var val = $("#main").attr("data-under");
+    $('[data-menuanchor$="' + val + '"]')
+      .first()
+      .addClass("active");
+  });
   $("#social-icons").load("components/social-icons.html");
 });
